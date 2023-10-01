b4765f80f9ad81e0cd1b2ba78d594adb3fee32f4
Brandi Burnell <brandiburnell6@gmail.com>
2023-09-30 20:40:43 -0600

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driversNames) {
+    return driversNames.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (driversNames) {
+    return driversNames.slice(2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare*multiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+//console.log(fareTripler(3));
+
+function selectDifferentDrivers(names, driverChoice) {
+    return driverChoice(names);
+}
